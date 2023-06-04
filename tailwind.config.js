e7a7f0593b80529f1a134fb939aa1a1c3d727032
ASIF\mahmu <Mohammad Mahmudul Islam>
2023-06-04 14:59:56 +0600

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,26 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      fontSize:{
+        "extra-large":["48px"]
+      },
+      colors:{
+        "primary":["#212121"],
+        "secondary":["#F5F5F5"],
+        "main":["#A36D3D"]
+      },
+      fontFamily:{
+        "poppins":["Poppins"]
+      },
+      backgroundImage:{
+        "background":"url('../images/img/restaurant-image.png')",
+        "background-two":"url('../images/img/bg.png')",
+        "background-hotel":"url('../images/hotel-image/hotel-image.png')"
+      },
+
+    },
+  },
+  plugins: [],
+}
